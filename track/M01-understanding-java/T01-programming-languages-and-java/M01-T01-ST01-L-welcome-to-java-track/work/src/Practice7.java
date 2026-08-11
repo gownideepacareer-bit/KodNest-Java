7c4278d32f4373b02d55e629f79565f42b23af6f
DEEPA RADHA <deeparadha@DEEPAs-MacBook-Air.local>
2026-08-11 10:54:55 +0530

    Practice7.java

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Practice7.java
@@ -0,0 +1,40 @@
+public class Practice7 {
+    public static void main(String[] args) {
+
+        int day = 3;
+
+        switch (day) {
+
+            case 1:
+                System.out.println("Holiday");
+                break;
+
+            case 2:
+                System.out.println("Learning day");
+                break;
+
+            case 3:
+                System.out.println("Learning day");
+                break;
+
+            case 4:
+                System.out.println("Learning day");
+                break;
+
+            case 5:
+                System.out.println("Learning day");
+                break;
+
+            case 6:
+                System.out.println("Revision Day");
+                break;
+
+            case 7:
+                System.out.println("Revision Day");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
+        }
+    }
+}
