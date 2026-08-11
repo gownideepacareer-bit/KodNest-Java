2812d46feb90554990babddddf71b1cb76f7aa50
DEEPA RADHA <deeparadha@DEEPAs-MacBook-Air.local>
2026-08-11 11:36:15 +0530

    Practice Task -8

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Practice8.java
@@ -0,0 +1,19 @@
+public class Practice8 {
+
+    public static void main(String[] args) {
+
+        int marks = 68;
+        int attendance = 80;
+        int practiceDays = 3;
+
+        String result = (marks >= 60 && attendance >= 75)
+                ? "Placement Ready"
+                : "Complete More Preparation";
+
+        System.out.println(result);
+
+        for (int i = 1; i <= practiceDays; i++) {
+            System.out.println("Practice Day: " + i);
+        }
+    }
+}
